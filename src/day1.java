c785d990db9bf604d820d5dc7701e597ebe33f0c
Hjalmar-Lundmark <lundmarkhjalmar@gmail.com>
2023-12-01 10:09:29 +0100

    Day1 part2 completed

--- src/day1.java
+++ src/day1.java
@@ -11,6 +11,9 @@ public class day1 {
         int firstInt = 0;
         int lastInt = 0;
         boolean part2 = true;
+        String threeLetter;
+        String fourLetter;
+        String fiveLetter;
 
         for (int i = 0; i < lines.length; i++) {
             // get first int
@@ -23,9 +26,25 @@ public class day1 {
                 }
                 // please close this, ugly solution
                 else if (part2) {
-                    String threeLetter = chars[j] + "" + chars[j+1] + chars[j+2];
-                    String fourLetter = chars[j] + "" + chars[j+1] + chars[j+2] + chars[j+3];
-                    String fiveLetter = chars[j] + "" + chars[j+1] + chars[j+2] + chars[j+3] + chars[j+4];
+                    if((j+2) >= chars.length) {
+                        threeLetter = "";
+                    } else {
+                        threeLetter = chars[j] + "" + chars[j+1] + chars[j+2];
+                    }
+
+                    if((j+3) >= chars.length) {
+                        fourLetter = "";
+                    } else {
+                        fourLetter = chars[j] + "" + chars[j+1] + chars[j+2] + chars[j+3];
+                    }
+
+                    if((j+4) >= chars.length) {
+                        fiveLetter = "";
+                    } else {
+                        fiveLetter = chars[j] + "" + chars[j+1] + chars[j+2] + chars[j+3] + chars[j+4];
+                    }
+
+
                     if (threeLetter.equals("one")) {
                         firstInt = 1;
                         break;
@@ -69,35 +88,51 @@ public class day1 {
                 }
                 // please close this, ugly solution
                 else if (part2) {
-                    String threeLetter = chars[j] + "" + chars[j+1] + chars[j+2];
-                    String fourLetter = chars[j] + "" + chars[j+1] + chars[j+2] + chars[j+3];
-                    String fiveLetter = chars[j] + "" + chars[j+1] + chars[j+2] + chars[j+3] + chars[j+4];
+                    if((j+2) >= chars.length) {
+                        threeLetter = "";
+                    } else {
+                        threeLetter = chars[j] + "" + chars[j+1] + chars[j+2];
+                    }
+
+                    if((j+3) >= chars.length) {
+                        fourLetter = "";
+                    } else {
+                        fourLetter = chars[j] + "" + chars[j+1] + chars[j+2] + chars[j+3];
+                    }
+
+                    if((j+4) >= chars.length) {
+                        fiveLetter = "";
+                    } else {
+                        fiveLetter = chars[j] + "" + chars[j+1] + chars[j+2] + chars[j+3] + chars[j+4];
+                    }
+
+
                     if (threeLetter.equals("eno")) {            //one
-                        firstInt = 1;
+                        lastInt = 1;
                         break;
                     } else if (threeLetter.equals("owt")) {     //two
-                        firstInt = 2;
+                        lastInt = 2;
                         break;
                     } else if (fiveLetter.equals("eerht")) {    //three
-                        firstInt = 3;
+                        lastInt = 3;
                         break;
                     } else if (fourLetter.equals("ruof")) {     //four
-                        firstInt = 4;
+                        lastInt = 4;
                         break;
                     } else if (fourLetter.equals("evif")) {     //five
-                        firstInt = 5;
+                        lastInt = 5;
                         break;
                     } else if (threeLetter.equals("xis")) {     //six
-                        firstInt = 6;
+                        lastInt = 6;
                         break;
                     } else if (fiveLetter.equals("neves")) {    //seven
-                        firstInt = 7;
+                        lastInt = 7;
                         break;
                     } else if (fiveLetter.equals("thgie")) {    //eight
-                        firstInt = 8;
+                        lastInt = 8;
                         break;
                     } else if (fourLetter.equals("enin")) {     //nine
-                        firstInt = 9;
+                        lastInt = 9;
                         break;
                     }
                 }
