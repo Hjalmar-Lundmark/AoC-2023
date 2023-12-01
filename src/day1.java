9fe05b6d715936df813358e242909ef8b7fc174e
Hjalmar-Lundmark <lundmarkhjalmar@gmail.com>
2023-12-01 09:31:17 +0100

    Day1 part1 complete

--- src/day1.java
+++ src/day1.java
@@ -1,22 +1,58 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.OptionalInt;
-import java.util.stream.IntStream;
 
 public class day1 {
     public static void main(String[] args) {
         String in = readFile("day1.txt");
         String[] lines = in.split(" ");
+        char[] chars;
         int total = 0;
         int firstInt = 0;
         int lastInt = 0;
 
-        System.out.println(lines[0]);
         for (int i = 0; i < lines.length; i++) {
+            // get first int
+            chars = lines[i].toCharArray();
+            for (int j = 0; j < chars.length; j++) {
+                if (isInt(chars[j])) {
+                    firstInt = Integer.parseInt(String.valueOf(chars[j]));
+                    System.out.println(firstInt);
+                    break;
+                }
+            }
 
+            // get second int
+            System.out.println(chars);
+            chars = reverse(chars);
+            System.out.println(chars);
+            for (int j = 0; j < chars.length; j++) {
+                if (isInt(chars[j])) {
+                    lastInt = Integer.parseInt(String.valueOf(chars[j]));
+                    System.out.println(lastInt);
+                    break;
+                }
+            }
+
+            total = total + (firstInt * 10) + lastInt;
+            System.out.println(total);
+        }
+        System.out.println(total);
+    }
+
+    public static char[] reverse(char[] c) {
+        char[] newArray = new char[c.length];
+        for (int i = 0; i < c.length; i++) {
+            newArray[i] = c[c.length-i-1];
+        }
+        return newArray;
+    }
+
+    public static boolean isInt(char c) {
+        if (c == '1' || c == '2' || c == '3' || c == '4' || c == '5' || c == '6' || c == '7' || c == '8' || c == '9' || c == '0') {
+            return true;
+        } else {
+            return false;
         }
     }
 
